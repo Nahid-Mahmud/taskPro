b26d1a31bd0dc26f41fd72d28d935c81935b659c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-06-14 16:34:29 +0600

    fontFamilly added

--- src/components/home/TaskForm.jsx
+++ src/components/home/TaskForm.jsx
@@ -42,7 +42,7 @@ const TaskForm = () => {
 
   return (
     <form onSubmit={onFormSubmit} className="max-w-lg mx-auto p-4 bg-secondary text-white shadow-md rounded-md ">
-      <h2 className="text-xl font-bold mb-4">Add a New Todo</h2>
+      <h2 className="text-xl font-bold mb-4">Add A New Task</h2>
       <div className="mb-4">
         <label className="block text-white text-sm font-bold mb-2" htmlFor="title">
           Title
@@ -53,7 +53,7 @@ const TaskForm = () => {
           onChange={(e) => setFormData({ ...formData, title: e.target.value })}
           id="title"
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="Enter todo title"
+          placeholder="Enter title"
           required={true}
         />
       </div>
@@ -66,7 +66,7 @@ const TaskForm = () => {
           value={formData.description}
           onChange={(e) => setFormData({ ...formData, description: e.target.value })}
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          placeholder="Enter todo description"
+          placeholder="Enter description"
           required={true}
         />
       </div>
@@ -77,7 +77,7 @@ const TaskForm = () => {
           disabled={isLoading}
           className="bg-[#c541e2] hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          Add Todo
+          Add Task
         </button>
       </div>
     </form>
